b6086e27d2b5b6df988de8177a72a0a656d26a70
sophienardi12 <enriconardi2@icloud.com>
2024-03-07 16:47:53 +0100

    versione giusta prof workshop

---
+++ src/components/CardPizza.svelte
@@ -0,0 +1,66 @@
+<script>
+	export let image
+	export let titolo
+	export let descrizione
+	export let prezzo
+	export let button
+</script>
+ <!-- card_Pizze -->
+    <div class="Card_pizze">
+      <div class="card_pizze__img">
+        <img src={image} alt="" />
+      </div>
+      <div class="card_pizze__centro">
+        <p>{titolo}</p>
+        <p class="Card_pizze__descrizione">{descrizione}</p>
+      </div>
+      <div class="Card_pizze__footer">
+        <p>{prezzo}</p>
+		 <div class="Card_pizze__button">
+        <p>{button}</p>
+		 </div>
+   </div>
+</div>
+
+<style>
+	
+ .Card_pizze {
+        background-color: #D7CBCB;
+        width: 300px;
+        border-radius: 20px;
+        padding: 30px;
+        font-style: normal;
+        font-size: 20px;
+        color: rgba(0, 0, 0, 0);
+      }
+	.card_pizze__img { 
+        color: rgba(0, 0, 0, 0);
+        height: 260px;
+        object-fit: cover;
+        aspect-ratio: 1;}
+
+	.card_pizze__centro {color: rgba(0, 0, 0, 0);}
+
+	.Card_pizze__descrizione {
+        color: rgba(0, 0, 0, 0);
+		font-size: 20px;
+		font-style: italic
+	}
+
+	.Card_pizze__footer {
+        color: rgba(0, 0, 0, 0);
+		 display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+	}
+ 
+	.Card_pizze__button { 
+		border-radius: 11px;
+        color: rgba(0, 0, 0, 0);
+    background: #E38416;
+	  width: 119.565px;
+    height: 24px;
+    flex-shrink: 0;}
+
+	
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,55 @@
 <script lang="ts">
-	import { get_collection } from '$cms';
+	import CardPizza from '$components/CardPizza.svelte';
+
+	let pizze = [
+		{
+			titolo: 'Pizza Margherita',
+			descrizione:
+				'composta da una base di pasta lievitata, generosamente condita con salsa di pomodoro fresca e leggera. Il tocco cremoso è dato dalla mozzarella, un formaggio a pasta filata, che si fonde splendidamente con il calore del forno.',
+			prezzo: '7 euro',
+			button: 'Add to cart',
+			image:
+				'https://it.ooni.com/cdn/shop/articles/Margherita-9920.jpg?crop=center&height=800&v=1644590028&width=800'
+		},
+		{
+			titolo: 'Pizza Diavola',
+			descrizione:
+				'condita con salsa di pomodoro leggermente piccante, mozzarella cremosa e fette di salame piccante che aggiungono un tocco di sapore intenso. ',
+			prezzo: '7 euro',
+			button: 'Add to cart',
+			image: 'https://img.cuisineaz.com/660x660/2022/06/30/i184454-pizza-diavola.jpeg'
+		},
+		{
+			titolo: 'Pizza 4 formaggi',
+			descrizione:
+				'mozzarella, gorgonzola, fontina e parmigiano reggiano, creando un mix di sapori cremosi, piccanti e intensi ',
+			prezzo: '7 euro',
+			button: 'Add to cart',
+			image:
+				'https://uploads-ssl.webflow.com/6001804d7e7aa9837e4eec83/6421b870c3a0c85a9d35a3f2_quattro-fromaggi-pizza.jpg'
+		},
+		{
+			titolo: 'Pizza speciale',
+			descrizione:
+				"prosciutto, funghi, peperoni, olive, cipolle, carciofi, salsiccia. creando un mix di sapori cremosi, piccanti e intensi che rendono questa pizza un'esperienza irresistibile per gli amanti del formaggio.",
+			prezzo: '7 euro',
+			button: 'Add to cart',
+			image:
+				'https://www.soniaperonaci.it/wp-content/uploads/2022/02/Pizza-con-mortadella-burrata-e-pesto-di-pistacchi-5.jpg'
+		}
+	];
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+
+{#each pizze as pizza}
+	<CardPizza
+		titolo={pizza.titolo}
+		descrizione={pizza.descrizione}
+		prezzo={pizza.prezzo}
+		button={pizza.button}
+		image={pizza.image}
+	/>
+{/each}
