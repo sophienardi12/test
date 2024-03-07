fc3b247ceb76b6b5b5bbe1cc93156523e077e973
sophienardi12 <162457520+sophienardi12@users.noreply.github.com>
2024-03-07 13:57:00 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Navbar from '$components/Navbar.svelte';
+	import '../styles.css';
+</script>
+
+<Navbar />
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { get_collection } from '$cms';
+</script>
+
+Home page
